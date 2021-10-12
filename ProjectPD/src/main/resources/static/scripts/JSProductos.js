17a281af1bd34230ffff5dc3b8ec8b676a45d010
matias <matias@DESKTOP-95T80HL>
2021-10-12 20:23:42 -0300

    ingresar

--- ProjectPD/src/main/resources/static/scripts/JSProductos.js
+++ ProjectPD/src/main/resources/static/scripts/JSProductos.js
@@ -6,21 +6,19 @@
 
 
 console.log("holQ");
- 
-document.querySelector('boton').addEvenListener('click',mostrarProductos);
 function mostrarProductos() {
     console.log('Dentro de la función');
 
     const xhttp = new XMLHttpRequest();
     xhttp.open('GET', '../scripts/JsonProductos.json', true);
-    xhttp.send();
     
     xhttp.onreadystatechange = function () {
 
         if (this.readyState == 4 && this.status == 200) {
             console.log(this.responseText);
-             
+            let datos = JSON.parse(this.responseText);
+            console.log(datos);
         }
 
     };
-    }
+}
