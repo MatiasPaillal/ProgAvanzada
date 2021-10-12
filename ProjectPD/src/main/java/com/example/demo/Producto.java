91d96a04c310167edc0c5ab51412d47d20970a92
matias <matias@DESKTOP-95T80HL>
2021-10-12 19:15:04 -0300

    ingresar

--- ProjectPD/src/main/java/com/example/demo/Producto.java
+++ ProjectPD/src/main/java/com/example/demo/Producto.java
@@ -4,12 +4,18 @@
  * and open the template in the editor.
  */
 package com.example.demo;
-
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 /**
  *
  * @author matias
  */
+@Entity
 public class Producto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private String id;
     private String nombre;
     private String categoria;
